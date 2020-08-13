78a3a2b31c8b0e0b5bf4e8ee68117c689677fbe0
elikesprogramming <elikesprogramming@gmail.com>
2020-08-13 17:28:45 +0200

    Fix after f100 -> sciwheel change

--- background.js
+++ background.js
@@ -1,13 +1,53 @@
-// 
+// We want to use chrome.pageAction to add a clickable extension button
+// to generate the annotations report
+//
+// https://developer.chrome.com/extensions/pageAction
+// Use the chrome.pageAction API to put icons in the main Google Chrome 
+// toolbar, to the right of the address bar. Page actions represent actions 
+// that can be taken on the current page, but that aren't applicable to all 
+// pages. Page actions appear grayed out when inactive.
+
+// The button should only be enabled when the current tab is on a specific item
+// from your schiweel library, either in the detail view or in the pdf view.
+// URLs for those two cases are formed as follows: 
+// https://sciwheel.com/work/#/items/9357944/detail
+// https://sciwheel.com/work/item/9357944/resources/9099136/pdf
+// (did not find documentation for this just what you can see browsing sciwheel)
+
+function getSciwheelRefId(tabUrl) {
+  refIdDetailView = tabUrl.match('(sciwheel.com/work/#/items/)(\\d+)');
+  refIdPdfView = tabUrl.match('(sciwheel.com/work/item/)(\\d+)');
+  // Matches either of those formats, or returns null
+  refId = refIdDetailView ?? refIdPdfView; // Nullish coalescing operator
+  refId = refId ? refId[2] : refId; // id are the numbers in the second group
+  console.log(tabUrl)
+  console.log(refId)
+  return refId;
+}
+
 function onWebNav(details) {
-  var refIndex = details.url.indexOf('#');
-  var ref = refIndex >= 0 ? details.url.slice(refIndex + 1) : '';
-  if ( (ref.indexOf('/items/') != -1 && ref.match('/\\d+') != null) || details.url.match('/work/item/\\d+') != null) { 
+
+  sciwheelRefId = getSciwheelRefId(details.url)
+  if (sciwheelRefId) { 
     chrome.pageAction.show(details.tabId);
   } else {
     chrome.pageAction.hide(details.tabId);
   }
-}s
+
+  /* Old implementation that I did not properly document at the time
+     but now with fresh eyes seems unnecessarily convoluted
+  var refIndex = details.url.indexOf('#');
+  var ref = refIndex >= 0 ? details.url.slice(refIndex + 1) : '';
+  if ( (ref.indexOf('/items/') != -1 && ref.match('/\\d+') != null) || 
+        details.url.match('/work/item/\\d+') != null) { 
+    chrome.pageAction.show(details.tabId);
+  } else {
+    chrome.pageAction.hide(details.tabId);
+  }
+  */
+}
+
+
 
 // Base filter to enable the extension button only con sciwheel.com pages
 var filter = {
@@ -30,15 +70,8 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 });
 
 chrome.pageAction.onClicked.addListener(function(activeTab) {
-  
-  // https://sciwheel.com/work/#/items/5821872?target=notes
-  // https://sciwheel.com/work/item/5821872/resources/4639906/pdf
 
-  // We match any number, and then we will get first the 1000 from sciwheel.com 
-  // and the second number in the array must be the article id
-  var regex = /[0-9]+/g;
-  var urlNumbers = activeTab.url.match(regex);
-  var sciwheelRefId = urlNumbers[1];
+  var sciwheelRefId = getSciwheelRefId(activeTab.url)
 
   chrome.storage.sync.set({'sciwheelRefId': sciwheelRefId}, function() {
     // Notify that we saved.
